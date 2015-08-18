e1522aeb9c40ea1ce89c5923d2117186e4585810
LYDIA FELICIA <lydiafeliciaa@gmail.com>
2015-08-18 01:52:37 -0400

    Adding title, newsletter form and it's jQuery

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 // 1. Create a welcome greeting to the user by taking the firstname and lastname the user input on the form/
-	function welcomeName( first, last ) {
+	/* function welcomeName( first, last ) {
 
 
 		return ("Hello " + first + " " + last + " welcome to the site! ");
@@ -22,12 +22,45 @@ $(document).ready(function() {
 		if (tweet.length < 140) {
 			alert("tweeted!");
 		} else {
-			alert ("too long!")
+			alert("too long!")
 		}
 
-	});
+	}); */
 
+//Newsletter
+$("#newsletter_button").click (function () {
+        $(".error").hide();
+
+        var hasError = false;
+        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+ 
+        var emailaddressVal = $("#useremail").val();
+        if(emailaddressVal == '') {
+            $("#useremail").after('<div class="error">Please enter your email address.</div>');
+            hasError = true;
+        }
+ 
+        else if(!emailReg.test(emailaddressVal)) {
+            $("#useremail").after('<div class="error">Enter a valid email address.</div>');
+            hasError = true;
+        }
+ 
+        if(hasError == true) { 
+        	return false;
+		} else {
+			$("#useremail").after('<div class="error">Thank you for joining our Newsletter!</div>');
+		}
+ });
+
+
+
+//Navbar slide
 	$("aside").slideDown(1000);
 
-
+//form fade-in
+	$("#form").fadeIn(2000);
 });
+    
+
+
+
